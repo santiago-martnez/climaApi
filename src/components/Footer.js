38d85a0a90c30105281e0e9104523111acb629ac
Santiago Martinez <zantumartinez@gmail.com>
2023-05-09 20:46:15 -0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return(
+     <footer class="text-center p-2 bg-dark">
+        <p class="text-white">05/05/2023. Hecho por Santiago Martinez 💻.</p>
+     </footer>
+    );
+}
+export default Footer;
---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+const NavBar = () => {
+    return(
+        <nav className="navbar bg-dark text-light mb-5">
+            <div className="container-fluid">
+                <h3 className="mx-auto">☀️Pronostico🌧️</h3>
+            </div>
+        </nav>
+    );
+}
+export default NavBar;
